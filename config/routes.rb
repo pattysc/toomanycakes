f994ae3ec289416943e08d64233970d67da35913
David Weinstein <txea@Davids-MacBook-Air.local>
2017-01-11 13:33:42 -0500

    added group stuff

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :groups, only: [:index, :show, :new, :create]
+  post 'groups/join', to: 'groups#join', as: 'join'
 
 
 

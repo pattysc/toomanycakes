ecf8da79322e1a41b821ce444726a91fa036c95a
pattysc <pattys@users.noreply.github.com>
2017-01-11 18:06:55 -0500

    added number_of_claimed_portions method

--- app/models/portion.rb
+++ app/models/portion.rb
@@ -1,4 +1,8 @@
 class Portion < ApplicationRecord
   belongs_to :meal
   belongs_to :eater, :class_name => 'User', optional: true
+
+  def self.number_of_claimed_portions(meal_id, user_id)
+    self.where(meal_id: meal_id, eater_id: user_id).count
+  end
 end

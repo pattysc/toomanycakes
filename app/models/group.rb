267c05d5182aaa78f37b1b7c08e49f99dc8f41ac
David Weinstein <txea@Davids-MacBook-Air.local>
2017-01-11 17:35:02 -0500

    some tests

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,11 +3,7 @@ class Group < ApplicationRecord
   has_many :users, through: :user_groups
   has_many :meals, through: :users
 
-
   def current_user_a_member?(current_user)
     users.include? current_user
   end
-
-
-
 end
